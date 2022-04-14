c6d10a13d69830aa94ed7a9dc1037bd62a6a167a
muyaho-universe <kimdaesk@gmai.com>
2022-04-14 21:57:52 +0900

    add 덧셈함수

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+int add(int a, intb){
+	return (a+b);
+}
+
 int main(){
 	int a, b;
 	printf("두 수 입력: ");
 	scanf("%d %d", &a, &b);
+	printf("add: %d\n" add(a,b));
 	return 0;
 }
